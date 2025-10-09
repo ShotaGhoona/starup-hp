b6d92129d88efa1a44df935938b0bde7e6e4283e
ShotaGhoona <shota.yamashita@ghoona.com>
2025-10-09 20:57:22 +0900

    feat: test-page

---
+++ src/app/test-planetary-clock/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { PlanetaryClock } from '@/components/animation/planetary-clock'
+
+export default function TestPlanetaryClockPage() {
+  return (
+    <PlanetaryClock />
+  )
+}
